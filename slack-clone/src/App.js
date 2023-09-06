14bc333ec16b8f48651c75a10322d34ce7e54245
Migi Castro <migicastro@yahoo.com>
2023-09-06 17:22:26 +0800

    added sidebar

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Sidebar */}
+      <div className='app_body'></div>
+      <Sidebar />
       {/* react router -> chat screen */}
     </div>
   );
---
+++ slack-clone/src/Sidebar/Sidebar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import CreateIcon from '@mui/icons-material/Create';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar_header'>
+            <div className='sidebar_info'>
+                <h2>Avion School</h2>
+                <h3>
+                    <FiberManualRecordIcon />
+                    Migi Castro
+                </h3>
+            </div>
+            <CreateIcon />
+        </div>
+        <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+        <SidebarOption Icon={AlternateEmailIcon} title='Mentions & reactions' />
+        <SidebarOption Icon={DraftsIcon} title='Saved items' />
+        <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+        <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+        <SidebarOption Icon={AppsIcon} title='Apps' />
+        <SidebarOption Icon={FileCopyIcon} title='File browser' />
+        <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+        <hr />
+        <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+        <SidebarOption Icon={AddIcon} title='Add Channel' />
+        <SidebarOption title='hi' />
+        {/* connect to DB and add all channels */}
+        <hr />
+        <SidebarOption Icon={ExpandMoreIcon} title='Direct Messages' />
+        <SidebarOption Icon={AddIcon} title='New Message' />
+        {/* connect to DB and add all DMs */}
+    </div>
+  )
+}
+
+export default Sidebar
